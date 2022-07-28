021ba1ba59d1c7179037ad9f7bb19c7ec082049a
wisdom obioma <wisdomobiomadev@gmail.com>
2022-07-28 13:41:59 +0100

    add background image assets

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import bgImg from "../../assets/images/bg.png";
+import bgImg from "../../assets/images/space-bg-2.png";
 
 export const StyledWrapper = styled.div`
   /* font-family: var(--font-stack-2-bold); */
