5a86570142b439ee3a0d505b93329457b683ee3e
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2022-07-28 19:57:10 +0100

    create audio player component

---
+++ src/components/AudioPlyer/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledAudioPlayer = styled.div`
+  position: fixed;
+  top: 0;
+  right: 10%;
+  height: 10em;
+  width: 10em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+`;
