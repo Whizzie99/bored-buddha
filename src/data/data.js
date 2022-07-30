708d44da1c5fa092d58efc2701d8827a5c2b490b
wisdom obioma <wisdomobiomadev@gmail.com>
2022-07-30 23:52:46 +0100

    compress assets

--- src/data/data.js
+++ src/data/data.js
@@ -1,33 +1,7 @@
-// nft images imports
-import nft1 from "../assets/images/nfts/1.jpg";
-import nft2 from "../assets/images/nfts/2.png";
-import nft3 from "../assets/images/nfts/3.png";
-import nft4 from "../assets/images/nfts/4.png";
-import nft5 from "../assets/images/nfts/5.png";
-import nft6 from "../assets/images/nfts/6.png";
-import nft7 from "../assets/images/nfts/7.png";
-import nft8 from "../assets/images/nfts/8.png";
-import nft9 from "../assets/images/nfts/9.png";
-import nft10 from "../assets/images/nfts/10.png";
-
 // utlity icons imports
 import utilityIcon1 from "../assets/icons/utility_icons/people.svg";
 import utilityIcon2 from "../assets/icons/utility_icons/diamond.svg";
 import utilityIcon3 from "../assets/icons/utility_icons/wellbeing.svg";
-
-// nft images arrays
-export const nftImages = [
-  nft1,
-  nft2,
-  nft3,
-  nft4,
-  nft5,
-  nft6,
-  nft7,
-  nft8,
-  nft9,
-  nft10,
-];
 
 // utlities array
 export const utlities = [
